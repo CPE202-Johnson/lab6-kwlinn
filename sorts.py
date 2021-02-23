e9a61804f31429bbd5ef7d7344c58f27b91ac904
kwlinn <kwlin@calpoly.edu>
2021-02-23 08:27:28 -0800

    test

--- sorts.py
+++ sorts.py
@@ -2,11 +2,25 @@ import random
 import time
 
 def selection_sort(list):
-    pass
+    count = 0
+    for i in range(0, len(list) - 1):
+        min_idx = i  
+        for j in range(i + 1, len(list)):
+            if list[j] < list[min_idx]:
+                min_idx = j
+            count += 1
+        list[min_idx], list[i] = list[i], list[min_idx]        
+    return count
     
 def insertion_sort(list):
-    pass
-   
+    count = 0
+    for i in range(1, len(list)):
+        value = list[i]
+        while list[i - 1] > value and i > 0:
+            list[i], list[i - 1] = list[i - 1], list[i]
+            i -= 1
+            count += 1
+    return count
 
 def main():
     # Code coverage NOT required for main
@@ -15,7 +29,7 @@ def main():
     random.seed(1234) 
     
     # Generate 5000 random numbers from 0 to 999,999
-    randoms = random.sample(range(1000000), 5000)
+    randoms = random.sample(range(1000000), 32000)
     start_time = time.time() 
     comps = selection_sort(randoms)
     stop_time = time.time()
